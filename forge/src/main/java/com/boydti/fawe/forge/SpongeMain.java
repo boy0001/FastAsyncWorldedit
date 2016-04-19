3ed58f0d3730b4e45a8236dbbba64633db2a551e
Jesse Boyd <jessepaleg@gmail.com>
2016-04-20 06:33:17 +1000

    Bump version

--- forge/src/main/java/com/boydti/fawe/forge/SpongeMain.java
+++ forge/src/main/java/com/boydti/fawe/forge/SpongeMain.java
@@ -18,7 +18,7 @@ import org.spongepowered.api.plugin.PluginContainer;
 import org.spongepowered.api.profile.GameProfileManager;
 import org.spongepowered.api.world.World;
 
-@Plugin(id = "com.boydti.fawe", name = "FastAsyncWorldEdit", description = "Lagless WorldEdit, Area restrictions, Memory mangement, Block logging", url = "https://github.com/boy0001/FastAsyncWorldedit", version = "3.3.16")
+@Plugin(id = "com.boydti.fawe", name = "FastAsyncWorldEdit", description = "Lagless WorldEdit, Area restrictions, Memory mangement, Block logging", url = "https://github.com/boy0001/FastAsyncWorldedit", version = "3.3.17")
 public class SpongeMain {
     public PluginContainer plugin;
 
