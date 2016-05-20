ba2f9159b68646dd437cc62d98d76095b9868074
Jesse Boyd <jessepaleg@gmail.com>
2016-05-20 23:49:09 +1000

    Get entities sync

--- core/src/main/java/com/boydti/fawe/wrappers/WorldWrapper.java
+++ core/src/main/java/com/boydti/fawe/wrappers/WorldWrapper.java
@@ -363,13 +363,23 @@ public class WorldWrapper extends AbstractWorld {
     }
 
     @Override
-    public List<? extends Entity> getEntities(Region region) {
-        return parent.getEntities(region);
+    public List<? extends Entity> getEntities(final Region region) {
+        return TaskManager.IMP.sync(new RunnableVal<List<? extends Entity>>() {
+            @Override
+            public void run(List<? extends Entity> value) {
+                this.value = parent.getEntities(region);
+            }
+        });
     }
 
     @Override
     public List<? extends Entity> getEntities() {
-        return parent.getEntities();
+        return TaskManager.IMP.sync(new RunnableVal<List<? extends Entity>>() {
+            @Override
+            public void run(List<? extends Entity> value) {
+                this.value = parent.getEntities();
+            }
+        });
     }
 
     @Override
