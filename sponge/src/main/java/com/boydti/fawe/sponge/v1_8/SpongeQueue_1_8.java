b209c6221aeaf5787560dffff3b5f486efda9b28
Jesse Boyd <jessepaleg@gmail.com>
2016-05-23 06:52:34 +1000

    *

--- sponge/src/main/java/com/boydti/fawe/sponge/v1_8/SpongeQueue_1_8.java
+++ sponge/src/main/java/com/boydti/fawe/sponge/v1_8/SpongeQueue_1_8.java
@@ -186,7 +186,7 @@ public class SpongeQueue_1_8 extends NMSMappedFaweQueue<World, net.minecraft.wor
 
     @Override
     public World getWorld(String world) {
-        return Sponge.getServer().getWorld(super.getWorld()).get();
+        return Sponge.getServer().getWorld(super.getWorldName()).get();
     }
 
     @Override
