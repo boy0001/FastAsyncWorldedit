09d8c60c00ff40204407cbe658fbc1ebef442346
Bogdan2402 <senior.bogdan1999@gmail.com>
2017-05-17 17:50:01 +0300

    check

---
+++ nukkit/src/main/java/com/boydti/fawe/nukkit/optimization/FaweNukkit.java
@@ -0,0 +1,152 @@
+package com.boydti.fawe.nukkit.optimization;
+
+import cn.nukkit.Player;
+import cn.nukkit.event.EventHandler;
+import cn.nukkit.event.Listener;
+import cn.nukkit.event.player.PlayerQuitEvent;
+import com.boydti.fawe.Fawe;
+import com.boydti.fawe.IFawe;
+import com.boydti.fawe.config.Settings;
+import com.boydti.fawe.nukkit.core.NukkitTaskManager;
+import com.boydti.fawe.nukkit.core.NukkitWorldEdit;
+import com.boydti.fawe.nukkit.listener.BrushListener;
+import com.boydti.fawe.nukkit.optimization.queue.NukkitQueue;
+import com.boydti.fawe.object.FaweChunk;
+import com.boydti.fawe.object.FaweCommand;
+import com.boydti.fawe.object.FawePlayer;
+import com.boydti.fawe.object.FaweQueue;
+import com.boydti.fawe.object.brush.visualization.VisualChunk;
+import com.boydti.fawe.regions.FaweMaskManager;
+import com.boydti.fawe.util.TaskManager;
+import com.sk89q.worldedit.world.World;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.UUID;
+import java.util.logging.Level;
+
+public class FaweNukkit implements IFawe, Listener {
+
+    private final NukkitWorldEdit plugin;
+
+    public FaweNukkit(NukkitWorldEdit mod) {
+        this.plugin = mod;
+        FaweChunk.HEIGHT = 256;
+        VisualChunk.VISUALIZE_BLOCK = 20 << 4;
+        plugin.getServer().getPluginManager().registerEvents(this, plugin);
+        new BrushListener(mod);
+    }
+
+    @EventHandler
+    public void onPlayerQuit(PlayerQuitEvent event) {
+        Player player = event.getPlayer();
+        FawePlayer fp = FawePlayer.wrap(player);
+        if (fp != null) {
+            fp.unregister();
+        }
+        Fawe.get().unregister(event.getPlayer().getName());
+    }
+
+    @Override
+    public void debug(String s) {
+        if (plugin.getWELogger() == null) {
+            System.out.println(s);
+        } else {
+            plugin.getWELogger().log(Level.INFO, s);
+        }
+    }
+
+    @Override
+    public File getDirectory() {
+        return plugin.getDataFolder();
+    }
+
+    @Override
+    public void setupCommand(String label, final FaweCommand cmd) {
+        plugin.getServer().getCommandMap().register(label, new NukkitCommand(label, cmd));
+    }
+
+    @Override
+    public FawePlayer wrap(Object obj) {
+        if (obj.getClass() == String.class) {
+            String name = (String) obj;
+            FawePlayer existing = Fawe.get().getCachedPlayer(name);
+            if (existing != null) {
+                return existing;
+            }
+            return new FaweNukkitPlayer(getPlugin().getServer().getPlayer(name));
+        } else if (obj instanceof Player) {
+            Player player = (Player) obj;
+            FawePlayer existing = Fawe.get().getCachedPlayer(player.getName());
+            return existing != null ? existing : new FaweNukkitPlayer(player);
+        } else {
+            return null;
+        }
+    }
+
+    public NukkitWorldEdit getPlugin() {
+        return plugin;
+    }
+
+    @Override
+    public void setupVault() {
+
+    }
+
+    @Override
+    public TaskManager getTaskManager() {
+        return new NukkitTaskManager(plugin);
+    }
+
+    @Override
+    public FaweQueue getNewQueue(World world, boolean fast) {
+        Settings.IMP.HISTORY.COMBINE_STAGES = false;
+        return new NukkitQueue(this, world);
+    }
+
+    @Override
+    public FaweQueue getNewQueue(String world, boolean fast) {
+        Settings.IMP.HISTORY.COMBINE_STAGES = false;
+        return new NukkitQueue(this, world);
+    }
+
+    @Override
+    public String getWorldName(World world) {
+        return world.getName();
+    }
+
+    @Override
+    public Collection<FaweMaskManager> getMaskManagers() {
+        return new ArrayList<>();
+    }
+
+    @Override
+    public void startMetrics() {
+        Metrics metrics = new Metrics(plugin);
+        metrics.start();
+    }
+
+    @Override
+    public String getPlatform() {
+        return "nukkit";
+    }
+
+    @Override
+    public UUID getUUID(String name) {
+        try {
+            return UUID.fromString(name);
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    @Override
+    public String getName(UUID uuid) {
+        return uuid.toString();
+    }
+
+    @Override
+    public Object getBlocksHubApi() {
+        return null;
+    }
+}
---
+++ nukkit/src/main/java/com/boydti/fawe/nukkit/optimization/queue/NukkitChunk.java
@@ -0,0 +1,141 @@
+package com.boydti.fawe.nukkit.optimization.queue;
+
+import cn.nukkit.blockentity.BlockEntity;
+import cn.nukkit.level.Level;
+import cn.nukkit.level.format.generic.BaseFullChunk;
+import cn.nukkit.nbt.tag.IntTag;
+import cn.nukkit.nbt.tag.Tag;
+import com.boydti.fawe.FaweCache;
+import com.boydti.fawe.example.CharFaweChunk;
+import com.boydti.fawe.nukkit.core.NBTConverter;
+import com.boydti.fawe.nukkit.core.NukkitUtil;
+import com.boydti.fawe.object.FaweQueue;
+import com.boydti.fawe.util.MainUtil;
+import com.sk89q.jnbt.CompoundTag;
+import com.sk89q.worldedit.LocalWorld;
+import com.sk89q.worldedit.MutableBlockVector2D;
+import java.io.IOException;
+import java.util.Map;
+
+public class NukkitChunk extends CharFaweChunk<BaseFullChunk, NukkitQueue> {
+
+
+    /**
+     * A FaweSections object represents a chunk and the blocks that you wish to change in it.
+     *
+     * @param parent
+     * @param x
+     * @param z
+     */
+    public NukkitChunk(FaweQueue parent, int x, int z) {
+        super(parent, x, z);
+    }
+
+    public NukkitChunk(FaweQueue parent, int x, int z, char[][] ids, short[] count, short[] air, byte[] heightMap) {
+        super(parent, x, z, ids, count, air, heightMap);
+    }
+
+    @Override
+    public CharFaweChunk copy(boolean shallow) {
+        NukkitChunk copy;
+        if (shallow) {
+            copy = new NukkitChunk(getParent(), getX(), getZ(), ids, count, air, heightMap);
+            copy.biomes = biomes;
+            copy.chunk = chunk;
+        } else {
+            copy = new NukkitChunk(getParent(), getX(), getZ(), (char[][]) MainUtil.copyNd(ids), count.clone(), air.clone(), heightMap.clone());
+            copy.biomes = biomes;
+            copy.chunk = chunk;
+            copy.biomes = biomes.clone();
+            copy.chunk = chunk;
+        }
+        return copy;
+    }
+
+    @Override
+    public BaseFullChunk getNewChunk() {
+        return ((NukkitQueue) getParent()).getWorld().getChunk(getX(), getZ(), true);
+    }
+
+    private int layer = -1;
+    private int index;
+    private boolean place = true;
+
+    @Override
+    public void start() {
+        try {
+            getChunk().load(true);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public NukkitChunk call() {
+        // Set heightmap
+        NukkitQueue parent = (NukkitQueue) getParent();
+        Level world = ((NukkitQueue) getParent()).getWorld();
+        world.clearCache(true);
+        final BaseFullChunk chunk = getChunk();
+        getParent().setHeightMap(this, heightMap);
+        char[][] sections = getCombinedIdArrays();
+        final int X = getX() << 4;
+        final int Z = getZ() << 4;
+        if (biomes != null) {
+            final LocalWorld lw = NukkitUtil.getLocalWorld(world);
+            final byte[] biomes = getBiomeArray();
+            int index = 0;
+            for (int z = 0; z < 16; z++) {
+                int zz = Z + z;
+                for (int x = 0; x < 16; x++) {
+                    int xx = X + x;
+                    lw.setBiome(MutableBlockVector2D.get(xx, zz), FaweCache.getBiome(biomes[index++] & 0xFF));
+                }
+            }
+        }
+        for (int layer = 0; layer < sections.length; layer++) {
+            char[] ids = sections[layer];
+            if (ids == null) {
+                continue;
+            }
+            int by = layer << 4;
+            int index = 0;
+            for (int y = by; y < by + 16; y++) {
+                for (int z = 0; z < 16; z++) {
+                    for (int x = 0; x < 16; x++,index++) {
+                        char combined = ids[index];
+                        switch (combined) {
+                            case 0:
+                                continue;
+                            case 1:
+                                chunk.setBlockId(x, y, z, 0);
+                                continue;
+                            default:
+                                int id = FaweCache.getId(combined);
+                                int data = FaweCache.getData(combined);
+                                chunk.setBlockId(x, y, z, id);
+                                chunk.setBlockData(x, y, z, data);
+                                if (FaweCache.hasNBT(id)) {
+                                    CompoundTag tile = getTile(x, y, z);
+                                    if (tile != null) {
+                                        cn.nukkit.nbt.tag.CompoundTag tag = (cn.nukkit.nbt.tag.CompoundTag) NBTConverter.toNative(tile);
+                                        String tileId = tag.getString("id");
+                                        Map<String, Tag> map = NBTConverter.getMap(tag);
+                                        map.put("x", new IntTag("x", X + x));
+                                        map.put("y", new IntTag("y", y));
+                                        map.put("z", new IntTag("z", Z + z));
+                                        BlockEntity ent = BlockEntity.createBlockEntity(tileId, chunk, tag);
+                                        if (ent != null) {
+                                            chunk.addBlockEntity(ent);
+                                        }
+                                        continue;
+                                    }
+                                }
+                        }
+                    }
+                }
+            }
+        }
+        return this;
+    }
+}
