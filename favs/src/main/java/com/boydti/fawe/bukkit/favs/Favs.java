e6e1a3c45f9da8d411e093b6f6d793bd6449ed4c
Jesse Boyd <jessepaleg@gmail.com>
2017-07-05 19:14:57 +1000

    update WE/VS auto-install jars

--- favs/src/main/java/com/boydti/fawe/bukkit/favs/Favs.java
+++ favs/src/main/java/com/boydti/fawe/bukkit/favs/Favs.java
@@ -1,10 +1,16 @@
 package com.boydti.fawe.bukkit.favs;
 
+import com.boydti.fawe.Fawe;
 import com.boydti.fawe.bukkit.BukkitCommand;
 import com.boydti.fawe.object.FaweCommand;
 import com.boydti.fawe.object.FawePlayer;
 import com.thevoxelbox.voxelsniper.SnipeData;
 import com.thevoxelbox.voxelsniper.Sniper;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.net.URL;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
@@ -15,6 +21,25 @@ public class Favs extends JavaPlugin {
     @Override
     public void onEnable() {
         try {
+            if (Bukkit.getPluginManager().getPlugin("VoxelSniper") == null) {
+                try {
+                    File output = new File(this.getDataFolder().getParentFile(), "VoxelSniper.jar");
+                    URL worldEditUrl = new URL("https://addons-origin.cursecdn.com/files/912/511/VoxelSniper-5.171.0-SNAPSHOT.jar");
+                    try (ReadableByteChannel rbc = Channels.newChannel(worldEditUrl.openStream())) {
+                        try (FileOutputStream fos = new FileOutputStream(output)) {
+                            fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+                        }
+                    }
+                    Bukkit.getPluginManager().loadPlugin(output);
+                } catch (Throwable e) {
+                    e.printStackTrace();
+                    Fawe.debug("====== INSTALL VOXELSNIPER ======");
+                    Fawe.debug("FAVS requires VoxelSniper to function correctly");
+                    Fawe.debug("Info: https://github.com/boy0001/FastAsyncWorldedit/releases/");
+                    Fawe.debug("===============================");
+                    return;
+                }
+            }
             SnipeData.inject();
             Sniper.inject();
             // Forward the commands so //p and //d will work
