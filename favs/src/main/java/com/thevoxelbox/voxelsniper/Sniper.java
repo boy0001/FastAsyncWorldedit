724bf55f135b5fad5329358fce55f168fb30d974
Jesse Boyd <jessepaleg@gmail.com>
2018-01-22 14:35:30 +1100

    Fixes #892

--- favs/src/main/java/com/thevoxelbox/voxelsniper/Sniper.java
+++ favs/src/main/java/com/thevoxelbox/voxelsniper/Sniper.java
@@ -1,3 +1,28 @@
+/**
+ This file is part of VoxelSniper, licensed under the MIT License (MIT).
+
+ Copyright (c) The VoxelBox <http://thevoxelbox.com>
+ Copyright (c) contributors
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+ */
+
 package com.thevoxelbox.voxelsniper;
 
 import com.boydti.fawe.Fawe;
@@ -360,15 +385,6 @@ public class Sniper {
                             @Override
                             public void run(Object value) {
                                 brush.perform(snipeAction, snipeData, targetBlock, lastBlock);
-                            }
-                        });
-                        break;
-                    case "WarpBrush":
-                        TaskManager.IMP.sync(new RunnableVal<Object>() {
-                            @Override
-                            public void run(Object value) {
-                                Block bukkitTargetBlock = bukkitWorld.getBlockAt(targetBlock.getLocation());
-                                brush.perform(snipeAction, snipeData, bukkitTargetBlock, lastBlock);
                             }
                         });
                         break;
