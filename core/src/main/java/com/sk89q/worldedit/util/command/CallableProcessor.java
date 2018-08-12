1b55aa0b0caad947dc2e838c4a42923d3cd4fa14
Jesse Boyd <jessepaleg@gmail.com>
2018-08-12 22:50:56 +1000

    Fixes #1070

--- core/src/main/java/com/sk89q/worldedit/util/command/CallableProcessor.java
+++ core/src/main/java/com/sk89q/worldedit/util/command/CallableProcessor.java
@@ -1,10 +1,12 @@
 package com.sk89q.worldedit.util.command;
 
-import com.plotsquared.general.commands.Command;
+import com.sk89q.minecraft.util.commands.CommandException;
 import com.sk89q.minecraft.util.commands.CommandLocals;
 import com.sk89q.worldedit.WorldEditException;
 
 public interface CallableProcessor<T> {
-    public Object process(CommandLocals locals, T value) throws Command.CommandException, WorldEditException;
+    public Object process(CommandLocals locals, T value) throws CommandException, WorldEditException;
+
+
 
 }
