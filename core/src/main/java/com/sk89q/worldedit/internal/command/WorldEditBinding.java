83e818d1c914c08161a6c4b16318e305489a37fe
Jesse Boyd <jessepaleg@gmail.com>
2018-08-22 00:43:30 +1000

    Potentially fix biome parsing

--- core/src/main/java/com/sk89q/worldedit/internal/command/WorldEditBinding.java
+++ core/src/main/java/com/sk89q/worldedit/internal/command/WorldEditBinding.java
@@ -20,6 +20,7 @@
 package com.sk89q.worldedit.internal.command;
 
 import com.boydti.fawe.object.FawePlayer;
+import com.boydti.fawe.util.MathMan;
 import com.sk89q.worldedit.EditSession;
 import com.sk89q.worldedit.IncompleteRegionException;
 import com.sk89q.worldedit.LocalSession;
@@ -322,6 +323,8 @@ public class WorldEditBinding extends BindingHelper {
     public BaseBiome getBiomeType(ArgumentStack context) throws ParameterException, WorldEditException {
         String input = context.next();
         if (input != null) {
+            if (MathMan.isInteger(input)) return new BaseBiome(Integer.parseInt(input));
+            
             Actor actor = context.getContext().getLocals().get(Actor.class);
             World world;
             if (actor instanceof Entity) {
